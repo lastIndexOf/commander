aee620648eb73a39afe0ab323394efa6d5e3b76d
zfkandu <kgaikj2cu@icloud.com>
2017-03-16 23:44:38 +0800

    add main program

--- gitsearch.js
+++ gitsearch.js
@@ -1,3 +1,42 @@
 #!/usr/bin/env node
+const program = require('commander')
+    , request = require('superagent')
+    , chalk = require('chalk')
 
-console.log('hello, CLI')
+program
+  .version('1.0.0')
+  .usage('<keywords>')
+  .parse(process.argv)
+
+if (!program.args.length) {
+  program.help()
+}
+else {
+  main()
+}
+
+function main() {
+  const keywords = program.args
+      , url = `https://api.github.com/search/repositories?sort=stars&order=desc&q=${ keywords }`
+
+  request.get(url)
+    .end((err, res) => {
+      if (err) 
+        return console.log(chalk.red(`Error: ${ err }`))
+      else {
+        loopPackage(res.body)
+      } 
+      
+    })
+}
+
+function loopPackage(packages) {
+  for (let key of Object.keys(packages)) {
+    const package = packages[key]
+    console.log(package)
+    // console.log(chalk.cyan.bold.underline(`Name: ${ package.name }`))
+    // console.log(chalk.cyan.bold.magenta.bold(`Owner: ${ package.owner.login }`))
+    // console.log(chalk.grey(`Desc: ${ package.description }`))
+    // console.log(chalk.grey(`Clone URL: ${ package.clone_url }`))
+  }
+}
